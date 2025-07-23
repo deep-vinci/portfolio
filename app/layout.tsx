b650b9e15e4b52a8ec06b00dea6de2b5c144ee75
Deepak <deepakworkpc@gmail.com>
2025-07-23 09:57:35 +0530

    feat: update names

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,31 +8,31 @@ import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
 
 export const metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: "Shivam's Portfolio",
-    template: "%s | Shivam's Portfolio",
-  },
-  description: "This is my portfolio.",
-  openGraph: {
-    title: "Shivam's Portfolio",
-    description: "This is my portfolio.",
-    url: baseUrl,
-    siteName: "Shivam's Portfolio",
-    locale: "en_US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+    metadataBase: new URL(baseUrl),
+    title: {
+        default: "deepvinci's Portfolio",
+        template: "%s | deepvinci's Portfolio",
     },
-  },
+    description: "This is my portfolio.",
+    openGraph: {
+        title: "deepvinci's Portfolio",
+        description: "This is my portfolio.",
+        url: baseUrl,
+        siteName: "Shivam's Portfolio",
+        locale: "en_US",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
