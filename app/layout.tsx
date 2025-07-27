2c57b8eba726a884a3922719f94e4830b43a9139
Deepak <deepakworkpc@gmail.com>
2025-07-27 17:51:13 +0530

    fix: og-image needs to be put in app/ not public/

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,34 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
 
+export const metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Shivam's Portfolio",
+    template: "%s | Shivam's Portfolio",
+  },
+  description: "This is my portfolio.",
+  openGraph: {
+    title: "Shivam's Portfolio",
+    description: "This is my portfolio.",
+    url: baseUrl,
+    siteName: "Shivam's Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
