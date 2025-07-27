92c44352bbcb4d3d3b5f8d00f7c6b3e645ab7796
Deepak <deepakworkpc@gmail.com>
2025-07-27 13:52:30 +0530

    feat: add a link to leetcode profile

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -41,17 +41,17 @@ export default function Footer() {
                         <p className="ml-2 h-7">github</p>
                     </a>
                 </li>
-                {/* <li>
+                <li>
                     <a
                         className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
                         rel="noopener noreferrer"
                         target="_blank"
-                        href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+                        href="https://leetcode.com/u/deepvinci"
                     >
                         <ArrowIcon />
-                        <p className="ml-2 h-7">view source</p>
+                        <p className="ml-2 h-7">leetcode</p>
                     </a>
-                </li> */}
+                </li>
             </ul>
             <p className="mt-8 text-neutral-600 dark:text-neutral-300">
                 © {new Date().getFullYear()} MIT Licensed
