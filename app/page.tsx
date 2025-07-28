7dcdfe25fb98a04f43786dd75db59d4c7a572528
Deepak <deepakworkpc@gmail.com>
2025-07-28 10:37:38 +0530

    feat: add Skills in page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ import Skills from "./components/skills";
 export default function Page() {
     return (
         <section>
-            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                deepvinci
+            <h1 className="mb-8 text-5xl font-extrabold tracking-tighter">
+                Hi, deepvinci here
             </h1>
             <p className="mb-4">
                 {`I'm a Fullstack web devloper  and engineering student who builds, functional systems with a purpose. Currently i am deep diving
                 into full stack devlopment, AI and Systems engineering.`}
             </p>
             <div className="my-8">
-                {/* <Skills /> */}
+                <Skills />
+            </div>
+
+            <div className="my-8">
                 <BlogPosts />
             </div>
         </section>
