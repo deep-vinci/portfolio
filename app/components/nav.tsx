d5cbd3a5af922c2629f7ecdd074c5647b5a531f6
Deepak <deepakworkpc@gmail.com>
2025-08-13 19:23:35 +0530

    feat: add social links

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Socials from "./socials";
 
 const navItems = {
     "/": {
@@ -15,9 +16,9 @@ const navItems = {
 export function Navbar() {
     return (
         <aside className="-ml-[10px] mb-16 sticky top-0 dark:bg-black bg-white tracking-tight">
-            <div className="lg:sticky  lg:top-20">
+            <div className="lg:sticky lg:top-20 ">
                 <nav
-                    className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+                    className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
                     id="nav"
                 >
                     <div className="flex flex-row space-x-0 pr-10">
@@ -33,6 +34,7 @@ export function Navbar() {
                             );
                         })}
                     </div>
+                    <Socials />
                 </nav>
             </div>
         </aside>
