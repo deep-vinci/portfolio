56323e76b08263b2c32e602c02b1155dbed050d9
deep-vinci <deepakworkpc@gmail.com>
2025-10-13 16:25:06 +0530

    chore: remove leetcode link

--- app/components/socials.tsx
+++ app/components/socials.tsx
@@ -41,17 +41,7 @@ export default function Socials() {
                         <p className="ml-2 h-7">github</p>
                     </a>
                 </li>
-                <li>
-                    <a
-                        className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        href="https://leetcode.com/u/deepvinci"
-                    >
-                        {/* <ArrowIcon /> */}
-                        <p className="ml-2 h-7">leetcode</p>
-                    </a>
-                </li>
+
                 <li>
                     <a
                         className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
